dd941e0e1c1fd71acd43abeb792dc0adf199dd4d
Alex <Alex@HDX.gateway.2wire.net>
2016-02-22 00:19:09 -0500

    Maybe the arm will hold itself up now?

---
+++ ArchBot/src/org/usfirst/frc3739/AuxBot/commands/ArmControl.java
@@ -0,0 +1,58 @@
+package org.usfirst.frc3739.AuxBot.commands;
+
+import org.usfirst.frc3739.AuxBot.Robot;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.command.Command;
+
+public class ArmControl extends Command {
+	private Joystick joystickC = Robot.oi.getJoystick('c');
+	private Joystick joystickD = Robot.oi.getJoystick('d');
+	private double shoulderPausePosition;
+	private double elbowPausePosition;
+
+	private static final double joystickDeadZone = 0.05;
+
+	public ArmControl() {
+		requires(Robot.arm);
+	}
+
+	// Called just before this Command runs the first time
+	protected void initialize() {
+		shoulderPausePosition = Robot.arm.getShoulderEncoderDistance();
+		elbowPausePosition = Robot.arm.getElbowEncoderDistance();
+	}
+
+	// Called repeatedly when this Command is scheduled to run
+	protected void execute() {
+		double joyCY = joystickC.getY(), joyDY = joystickD.getY();
+
+		if (joyCY > joystickDeadZone || joyCY < -joystickDeadZone) {
+			Robot.arm.setShoulderJointSpeed(-joyCY);
+			shoulderPausePosition = Robot.arm.getShoulderEncoderDistance();
+		} else {
+			Robot.arm.setShoulderJointPosition(shoulderPausePosition);
+		}
+		if (joyDY > joystickDeadZone || joyDY < -joystickDeadZone) {
+			Robot.arm.setElbowJointSpeed(joyDY);
+			elbowPausePosition = Robot.arm.getElbowEncoderDistance();
+		} else {
+			Robot.arm.setElbowJointPosition(elbowPausePosition);
+		}
+	}
+
+	// Make this return true when this Command no longer needs to run execute()
+	protected boolean isFinished() {
+		return false;
+	}
+
+	// Called once after isFinished returns true
+	protected void end() {
+	}
+
+	// Called when another command which requires one or more of the same
+	// subsystems is scheduled to run
+	protected void interrupted() {
+		end();
+	}
+}
--- ArchBot/src/org/usfirst/frc3739/AuxBot/subsystems/CanadArm.java
+++ ArchBot/src/org/usfirst/frc3739/AuxBot/subsystems/CanadArm.java
@@ -1,13 +1,12 @@
 package org.usfirst.frc3739.AuxBot.subsystems;
 
-import org.usfirst.frc3739.AuxBot.commands.OperateArm;
+import org.usfirst.frc3739.AuxBot.commands.ArmControl;
 
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.VictorSP;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  * The Arm subsystem will include the shoulder and elbow joint actuators of the
@@ -18,26 +17,24 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  * @version 1.0.0a
  */
 public class CanadArm extends Subsystem {
-	//private Servo testServo;
+
 	private VictorSP shoulderJoint;
 	private VictorSP elbowJoint;
 	private Encoder shoulderEncoder;
 	private Encoder elbowEncoder;
-	//private PIDController testPID;
-	final double pGain = 0, iGain = 0, dGain = 0; 
+
+	private static final double jointCompensateSpeed = 0.05;
+
 	public CanadArm() {
 		// Declarations
-		//testServo = new Servo(9);
 		shoulderJoint = new VictorSP(2);
 		elbowJoint = new VictorSP(3);
 		shoulderEncoder = new Encoder(9, 8, true);
 		shoulderEncoder.setDistancePerPulse(0.27211);
 		elbowEncoder = new Encoder(7, 6, true);
 		elbowEncoder.setDistancePerPulse(0.27211);
-		//testPID = new PIDController(pGain, iGain, dGain, testEncoder, testJoint);
 
 		// Displaying these in the LiveWindow
-		//LiveWindow.addActuator("Arm", "Test Servo", testServo);
 		LiveWindow.addActuator("Arm", "Shoulder Joint", shoulderJoint);
 		LiveWindow.addActuator("Arm", "Elbow Joint", elbowJoint);
 		LiveWindow.addSensor("Arm", "Shoulder Encoder", shoulderEncoder);
@@ -45,16 +42,16 @@ public class CanadArm extends Subsystem {
 	}
 
 	public void initDefaultCommand() {
-		setDefaultCommand(new OperateArm());
+		setDefaultCommand(new ArmControl());
 	}
 
 	public void log() {
 	}
-	
+
 	public double getShoulderEncoderDistance() {
 		return shoulderEncoder.getDistance();
 	}
-	
+
 	public double getElbowEncoderDistance() {
 		return elbowEncoder.getDistance();
 	}
@@ -62,56 +59,27 @@ public class CanadArm extends Subsystem {
 	public void setShoulderJointSpeed(double speed) {
 		shoulderJoint.set(speed);
 	}
+
 	public void setElbowJointSpeed(double speed) {
 		elbowJoint.set(speed);
 	}
-	/**
-	 * Rotates the joint to the specified degree.
-	 *
-	 * @param degrees The amount for the joint to rotate
-	 */
-	public void rotateShoulderJoint(double degrees) {
-		double firstDist = shoulderEncoder.getDistance();
-		double currDist = firstDist;
 
-		if (degrees > 0) {
-			while (currDist < firstDist + degrees) {
-				shoulderJoint.set(0.5);
-				currDist = shoulderEncoder.getDistance();
-				SmartDashboard.putNumber("Motor Raw", shoulderJoint.getRaw());
-				SmartDashboard.putNumber("Current Distance", currDist);
-			}
-			shoulderJoint.set(0);
-			SmartDashboard.putNumber("Motor Raw", shoulderJoint.getRaw());
-		}
-		else if (degrees < 0) {
-			while (currDist > firstDist + degrees) {
-				shoulderJoint.set(-0.5);
-				currDist = shoulderEncoder.getDistance();
-				SmartDashboard.putNumber("Current Distance", currDist);
-				SmartDashboard.putNumber("Motor Raw", shoulderJoint.getRaw());
-			}
-			shoulderJoint.set(0);
-			SmartDashboard.putNumber("Motor Raw", shoulderJoint.getRaw());
-		}
-		else shoulderJoint.set(0);
-		
-		//testServo.setAngle(degrees);
-	}
-	public void setJoints(double shoulderStopLocation, double elbowStopLocation) {
-		if (shoulderEncoder.getDistance() < shoulderStopLocation) {
-			shoulderJoint.set(-0.4);
-		}
-		else if (shoulderEncoder.getDistance() > shoulderStopLocation) {
-			shoulderJoint.set(0.4);
-		}
-		if (elbowEncoder.getDistance() < elbowStopLocation) {
-			elbowJoint.set(-0.4);
-		}
-		else if (elbowEncoder.getDistance() > elbowStopLocation) {
-			elbowJoint.set(0.4);
+	public void setShoulderJointPosition(double shoulderSetpoint) {
+		if (shoulderEncoder.getDistance() < shoulderSetpoint) {
+			shoulderJoint.set(jointCompensateSpeed);
+		} else if (shoulderEncoder.getDistance() > shoulderSetpoint) {
+			shoulderJoint.set(-jointCompensateSpeed);
 		}
 	}
+
+	public void setElbowJointPosition(double elbowSetpoint) {
+		if (elbowEncoder.getDistance() < elbowSetpoint) {
+			elbowJoint.set(jointCompensateSpeed);
+		} else if (elbowEncoder.getDistance() > elbowSetpoint) {
+			elbowJoint.set(-jointCompensateSpeed);
+		}
+	}
+
 	public void killJoints() {
 		shoulderJoint.set(0);
 		elbowJoint.set(0);
